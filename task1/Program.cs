7ee654c7ab2e7ea53b327ce30facdd0e43eab660
arina9999 <aacher9@mail.ru>
2022-11-30 00:06:12 +0600

    task0

---
+++ task1/Program.cs
@@ -0,0 +1,23 @@
+﻿// Задача №1. Напишите программу, которая на вход принимает два числа и проверяет, является ли первое число 
+//квадратом второго.
+// a = 25, b = 5 -> да 
+// a = 2, b = 10 -> нет 
+// a = 9, b = -3 -> да 
+// a = -3 b = 9 -> нет
+
+Console.WriteLine("Введите число a: ");
+int numbera = int.Parse(Console.ReadLine());
+Console.WriteLine("Введите число b: ");
+int numberb = int.Parse(Console.ReadLine());
+
+if (numbera == numberb * numberb)
+{
+    Console.WriteLine("YES");
+}
+else 
+{
+    Console.WriteLine("NO");
+}
+
+
+
---
+++ task3/Program.cs
@@ -0,0 +1,40 @@
+﻿// Задача 3. Напишите программу, которая будет выдавать название дня недели по заданному номеру
+// 3 - среда
+// 5 - пятница
+
+Console.WriteLine("Введите день недели от 1 до 7: ");
+int number = int.Parse(Console.ReadLine());
+
+
+if (number == 1)
+{
+    Console.WriteLine("1й день недели - Понедельник");
+}
+if (number == 2)
+{
+    Console.WriteLine("2й день недели - Вторник");
+}
+if (number == 3)
+{
+    Console.WriteLine("3й день недели - Среда");
+}
+if (number == 4)
+{
+    Console.WriteLine("4й день недели - Четверг");
+}
+if (number == 5)
+{
+    Console.WriteLine("5й день недели - Пятница");
+}
+if (number == 6)
+{
+    Console.WriteLine("6й день недели - Суббота");
+}
+if (number == 7)
+{
+    Console.WriteLine("7й день недели - Воскресенье");
+}
+else 
+{
+    Console.WriteLine("Такого дня недели не существует :)");
+}
