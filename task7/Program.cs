d1cf37293e690e355184b002f7952a6a22c01a9a
arina9999 <aacher9@mail.ru>
2022-11-30 13:14:37 +0600

    task7

--- task7/Program.cs
+++ task7/Program.cs
@@ -2,3 +2,19 @@
 // 456 -> 6
 // 782 -> 2
 // 918 -> 8
+
+Console.Write("Введите любое трехзначное число: ");
+int N = int.Parse(Console.ReadLine());
+
+int length = N.ToString().Length;
+int result = 0;
+
+if (length == 3)
+{
+    result = N % 10;
+    Console.WriteLine(result);
+}
+else 
+{
+    Console.WriteLine("Введите трехзначное число");
+}
