1d942cb78904fed5cf988d25a86a98a74faee9e6
arina9999 <aacher9@mail.ru>
2022-12-05 14:37:18 +0600

    task13

---
+++ task13/Program.cs
@@ -0,0 +1,28 @@
+﻿// Напишите программу, которая выводит третью
+// цифру заданного числа или сообщает,
+// что третьей цифры нет.
+
+Console.WriteLine("Введите любое число:");
+int num = Math.Abs(int.Parse(Console.ReadLine()));
+int numTemp = num;
+int count = 0;
+
+while (numTemp > 0)
+{
+    numTemp /= 10;
+    count++;
+}
+
+if (count - 3 >= 0)
+{
+    while (count - 3 > 0)
+    {
+        num = num / 10;
+        count--;
+    }
+    Console.WriteLine($"Третья цифра случайнго числа {num % 10}");
+}
+else
+{
+    Console.WriteLine("Третьей цифры здесь нет");
+}
