37f10b86b4c91c4819206186d12581af85296f39
arina9999 <aacher9@mail.ru>
2022-12-10 11:44:06 +0600

    task24

---
+++ task26/Program.cs
@@ -0,0 +1,21 @@
+﻿// Напишите программу, которая принимает на вход число 
+// и выдаёт количество цифр в числе. 
+// 456 -> 3 
+// 78 -> 2 
+// 89126 -> 5
+
+Console.Clear();
+Console.Write("Введите число: ");
+int numberTemp = int.Parse(Console.ReadLine());
+Console.WriteLine($"Количество цифр в числе = {GetCount(numberTemp)}");
+
+int GetCount(int numberTemp)
+{
+    int count = 0;
+    while (numberTemp > 0)
+    {
+        numberTemp /= 10;
+        count ++;
+    }
+    return count;
+}
