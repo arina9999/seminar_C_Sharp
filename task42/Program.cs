5fb3af568c8c525b1139b336b26320cff7316cc5
arina9999 <aacher9@mail.ru>
2022-12-21 12:26:43 +0600

    task42

---
+++ task42/Program.cs
@@ -0,0 +1,20 @@
+﻿// Задача 42: Напишите программу, которая будет 
+// преобразовывать десятичное число в двоичное.
+// 45 -> 101101
+// 3 -> 11
+// 2 -> 10
+
+Console.WriteLine("Введите число:");
+int num = int.Parse(Console.ReadLine()!);
+Console.WriteLine($"Число {num} конвертировали в двоичное");
+
+string numDouble = String.Empty;
+string numDoubleRev = String.Empty;
+while (true)
+{
+int res = num%2;
+numDouble = res + numDouble;
+num/=2;
+if(num == 0) break;
+}
+Console.Write(numDouble);
